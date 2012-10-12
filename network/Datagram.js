787462c9ccbcba464da6b85bee1e9e88f349eb93
Sacha Bron <sacha.bron@copperflake.com>
2012-10-12 13:50:41 +0200

    Avancée avec Socket.io

---
+++ network/Datagram.js
@@ -0,0 +1,13 @@
+function Datagram(from, type, playload) {
+  this.from = from
+  this.type = type
+  this.playload = playload
+}
+
+Datagram.prototype.send = function(to) {
+  
+}
+
+Datagram.prototype.broadcast = function() {
+
+}
--- server.js
+++ server.js
@@ -1,21 +1,48 @@
 console.log("Starting Node Server...")
 
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+
+var http = require('http')
+var server = http.createServer(app)
+
+var io = require('socket.io').listen(server)
+var clients = []
+
+var util = require('util');
 
 var views = "./view"
 var resources = "./resources"
 
-app.get('/', function(req, res){
-  res.sendfile(views+'/canvas.html');
-});
+server.listen(9000)
 
-app.get('/resources/*', function(req, res){
-  res.sendfile('./'+req.path);
-});
+app.get('/', function (req, res) {
+  res.sendfile(views+'/canvas.html')
+})
 
-app.get('/*', function(req, res){
-  res.sendfile(views+req.path);
-});
+app.get('/socket', function (req, res) {
+  res.sendfile(views+'/socket.html')
+})
 
-app.listen(9000);
+app.get('/resources/*', function (req, res) {
+  res.sendfile('./'+req.path)
+})
+
+app.get('/*', function(req, res) {
+  res.sendfile(views+req.path)
+})
+
+io.sockets.on('connection', function (socket) {
+  clients[socket.id] = socket
+  console.log("New connection:\n  ID: "+socket.id+"\n  IP: "+socket.handshake.address.address + ":" + socket.handshake.address.port+"\n");
+  
+  socket.broadcast.emit('new-player', {})
+  
+  socket.on('my other event', function (data) {
+    console.log(data)
+  })
+  
+  socket.on('disconnect', function() {
+    clients.splice(socket.id, 1)
+  })
+})
