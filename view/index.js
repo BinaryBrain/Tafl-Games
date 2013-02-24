51c5a9c94d859d477c9810d755973049ce99a531
Sacha Bron <sacha.bron@copperflake.com>
2013-02-24 01:52:52 +0100

    Adding a Notification system. Totally working ;)

--- view/index.js
+++ view/index.js
@@ -8,7 +8,19 @@ $(function () {
     showTeams();
   })
   
-  showGames();
+  $("#teamMaking #readyButton").live("click", function () {
+    alert("Launching the game!")
+  })
+  
+  $("#notifs .close").live("click", function () {
+    var notif = $(this).parent()
+    notif.animate({ left: -500 }, function () {
+      notif.remove()
+    })
+  })
+  
+  showGames()
+  welcomeNotifs()
 })
 
 function showGames() {
@@ -21,4 +33,25 @@ function showTeams() {
   $.get("/teams.html", function (data) {
     $("#frame").html(data)
   })
+}
+
+function newNotif(title, content) {
+  var id = Math.floor(Math.random()*10000)+""
+  var html = '<div class="notif" id="'+id+'"><div class="close"></div><h3>'+title+'</h3><p>'+content+'</p></div>'
+  $("#notifs").append(html)
+  $("#"+id).animate({ left: 0 })
+}
+
+function welcomeNotifs() {
+  setTimeout(function () {
+    newNotif("Hello!", "Welcome to this wonderful online gaming plateforme!")
+  }, 5000)
+  
+  setTimeout(function () {
+    newNotif("Games", "The main frame show you every games you can play on this web site with your friends.")
+  }, 8000)
+  
+  setTimeout(function () {
+    newNotif("Notifications", "Oh! By the way, I'm a notification. You may see me when I will have some informations for you.")
+  }, 13000)
 }
